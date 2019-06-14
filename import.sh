92dc2e13411903b57f6f84ea257eca32c664a8cf
Dmitri Saharov <dmitri.sah@gmail.com>
2019-06-14 13:34:29 -0400

    Allow hosting of multiple studies at once

--- import.sh
+++ import.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-cp -r $1/* ./
 mkdir study || true
-mkdir resources || true
-mv index.html $2.html
-mv ./*.js ./study/
-mv ./*.html ./study/
-find ./study/* | xargs -L 1 sed -i -e 's/https:\/\/lib.pavlovia.org\/psychojs.css/.\/css\/psychojs.css/g'
-find ./study/* | xargs -L 1 sed -i -e 's/https:\/\/pavlovia\.org\/lib\//\.\/js\//g'
-find ./study/* | xargs -L 1 sed -i -e 's/https:\/\/lib\.pavlovia\.org\//\.\/js\//g'
-find ./study/* | xargs -L 1 sed -i -e 's/debug: true/debug: false/g'
-find study/*.js | xargs -L 1 -I {} bash -c "find resources/* | xargs ./add_resources.py resources/ {}"
+mkdir ./study/$2/
+cd study/$2
+cp -r $1/* .
+find ./*.* | xargs -L 1 sed -i -e 's/https:\/\/lib.pavlovia.org\/psychojs.css/.\/css\/psychojs.css/g'
+find ./*.* | xargs -L 1 sed -i -e 's/https:\/\/pavlovia\.org\/lib\//\.\/js\//g'
+find ./*.* | xargs -L 1 sed -i -e 's/https:\/\/lib\.pavlovia\.org\//\.\/js\//g'
+find ./*.* | xargs -L 1 sed -i -e 's/debug: true/debug: false/g'
+find ./*.js | xargs -L 1 -I {} bash -c "find resources/* | xargs ../../add_resources.py resources/ {}"
--- serve.py
+++ serve.py
@@ -5,23 +5,23 @@ import json
 
 def init():
 
-    app = Flask(__name__, static_url_path='')
+    app = Flask(__name__, static_url_path='', static_folder='psychojs')
 
-    @app.route('/study/js/<path:path>')
-    def send_js(path):
+    @app.route('/study/<name>/js/<path:path>')
+    def send_js(name, path):
         return send_from_directory('psychojs', path)
 
-    @app.route('/study/css/<path:path>')
-    def send_css(path):
+    @app.route('/study/<name>/css/<path:path>')
+    def send_css(name, path):
         return send_from_directory('css', path)
 
-    @app.route('/study/resources/<path:path>')
-    def send_resource(path):
-        return send_from_directory('resources', path)
+    @app.route('/study/<study>/')
+    def send_study(study):
+        return send_from_directory('study/{}/'.format(study), 'index.html')
 
-    @app.route('/study/<path:path>')
-    def send_study(path):
-        return send_from_directory('study', path)
+    @app.route('/study/<study>/<path:path>')
+    def send_study_files(study, path):
+        return send_from_directory('study/{}'.format(study), path)
 
     return app
 
