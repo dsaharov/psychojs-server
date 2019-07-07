d81267f3df255d47c39e3b4dea6d952d093447b7
Dmitri Saharov <dmitri.sah@gmail.com>
2019-07-06 23:14:22 -0400

    Added simple auth for admin page

---
+++ auth.py
@@ -0,0 +1,44 @@
+from flask import session
+from datetime import datetime
+import json
+
+# WARNING: this is a homebrew authentication scheme
+# that may not be sufficient for apps exposed to the internet
+class SimpleSessionAuth():
+
+    def __init__(self, users):
+        self.session_keys = set()
+        self.users = users
+
+    def add_auth(self, user):
+        key = json.dumps({
+            'user': user,
+            'time': str(datetime.now())
+        })
+        self.session_keys.add(key)
+        session['auth'] = key
+        return key
+
+    def revoke_auth(self, key):
+        if key is not None:
+            self.session_keys.remove(key)
+            session.pop('auth', None)
+
+    def get_session_auth(self):
+        return session.get('auth', None)
+
+    def revoke_session_auth(self):
+        self.revoke_auth(self.get_session_auth())
+
+    def is_session_authenticated(self):
+        key = self.get_session_auth()
+        if key is not None and key not in self.session_keys:
+            self.revoke_auth(key)
+            key = None
+        return key is not None
+
+    def authenticate(self, user, passwd):
+        if user in self.users and self.users[user] == passwd:
+            self.add_auth(user)
+            return True
+        return False
--- serve.py
+++ serve.py
@@ -1,31 +1,63 @@
 #!/usr/bin/env python3
 from waitress import serve
 from flask import Flask, send_from_directory, render_template, jsonify, \
-    request, abort
+    request, abort, session, redirect, url_for
 from experiment_server import PsychoJsExperiment
 import json
 import os
 from logger import log
+import hashlib
+import datetime
+from auth import SimpleSessionAuth
 
-experiments = {}
+
+# Generate a key for signing session cookies
+SECRET_KEY = hashlib.sha224(
+    str(datetime.datetime.now()).encode(encoding='UTF-8')
+).hexdigest()
 DATA_PATH = './data/'
+USERS = {
+    'admin': 'defaulto passwordo'
+}
+
 
 def init():
-
     app = Flask(__name__, static_url_path='', static_folder='psychojs')
+    app.secret_key = SECRET_KEY
+
+    experiments = {}
+    auth = SimpleSessionAuth(USERS)
 
     if not os.path.exists(DATA_PATH):
         log('Creating data directory at {}'.format(DATA_PATH))
         os.makedirs(DATA_PATH)
 
-    @app.route('/study/<name>/js/<path:path>')
-    def send_js(name, path):
+    @app.route('/manage/logout')
+    def logout():
+        auth.revoke_session_auth()
+        return redirect(url_for('manage_view'))
+
+    @app.route('/manage/', methods=['GET', 'POST'])
+    def manage_view():
+        if 'user' in request.values and 'pass' in request.values:
+            auth.authenticate(
+                request.values['user'],
+                request.values['pass']
+            )
+
+        if auth.is_session_authenticated():
+            return send_from_directory('admin/', 'manage.html')
+        else:
+            return send_from_directory('admin/', 'login.html')
+
+    @app.route('/study/<study>/js/<path:path>')
+    def send_js(study, path):
         if study not in experiments:
             abort(404)
         return send_from_directory('psychojs', path)
 
-    @app.route('/study/<name>/css/<path:path>')
-    def send_css(name, path):
+    @app.route('/study/<study>/css/<path:path>')
+    def send_css(study, path):
         if study not in experiments:
             abort(404)
         return send_from_directory('css', path)
