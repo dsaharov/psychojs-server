bedcb9b3c91a923215d1774b7abc0540e49d4944
Dmitri Saharov <dmitri.sah@gmail.com>
2019-07-07 01:53:59 -0400

    Added option to deny unauthed study access

--- serve.py
+++ serve.py
@@ -20,6 +20,8 @@ USERS = {
     'admin': 'default'
 }
 
+ALLOW_UNAUTHED_STUDY_ACCESS = False
+
 
 def init():
     app = Flask(__name__, static_url_path='', static_folder='psychojs')
@@ -90,18 +92,27 @@ def init():
 
     @app.route('/study/<study>/js/<path:path>')
     def send_js(study, path):
+        if not ALLOW_UNAUTHED_STUDY_ACCESS and \
+                not auth.is_session_authenticated():
+            abort(404)
         if not exp_server.has_study(study):
             abort(404)
         return send_from_directory('psychojs', path)
 
     @app.route('/study/<study>/css/<path:path>')
     def send_css(study, path):
+        if not ALLOW_UNAUTHED_STUDY_ACCESS and \
+                not auth.is_session_authenticated():
+            abort(404)
         if not exp_server.has_study(study):
             abort(404)
         return send_from_directory('css', path)
 
     @app.route('/study/<study>/')
     def send_study(study):
+        if not ALLOW_UNAUTHED_STUDY_ACCESS and \
+                not auth.is_session_authenticated():
+            abort(404)
         if not exp_server.has_study(study):
             abort(404)
         study_path = exp_server.get_path(study)
@@ -110,12 +121,18 @@ def init():
 
     @app.route('/study/<study>/config.json')
     def send_study_config(study):
+        if not ALLOW_UNAUTHED_STUDY_ACCESS and \
+                not auth.is_session_authenticated():
+            abort(404)
         if not exp_server.has_study(study):
             abort(404)
         return jsonify(exp_server.get_config(study))
 
     @app.route('/study/<study>/server/', methods=['GET', 'POST'])
     def study_server(study):
+        if not ALLOW_UNAUTHED_STUDY_ACCESS and \
+                not auth.is_session_authenticated():
+            abort(404)
         if not exp_server.has_study(study):
             abort(404)
         response = exp_server.handle_request(study, request)
@@ -123,6 +140,9 @@ def init():
 
     @app.route('/study/<study>/<path:path>')
     def send_study_files(study, path):
+        if not ALLOW_UNAUTHED_STUDY_ACCESS and \
+                not auth.is_session_authenticated():
+            abort(404)
         if not exp_server.has_study(study):
             abort(404)
         study_path = exp_server.get_path(study)
